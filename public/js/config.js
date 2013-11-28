96b69a27c647406ed853fff49637a2f5ab9674af
Jason Oster <jay@kodewerx.org>
2013-11-27 21:50:04 -0800

    Fixes for GameMix banner
    - Do not show banner when debug mode is enabled
    - Better constant handling in config.js
    - Add a hash parser for runtime configuration:
        #debug&level=stage3
        Creates a c.HASH object that contains:
        { "debug" : "", "level" : "stage3" }

--- public/js/config.js
+++ public/js/config.js
@@ -2,11 +2,35 @@
 
     var enabled = false;
 
+    function parseHash() {
+        var hash = {};
+        document.location.hash.substr(1).split("&").filter(function (value) {
+            return (value !== "");
+        }).forEach(function (value) {
+            var kv = value.split("=");
+            var k = kv.shift();
+            var v = kv.join("=");
+            hash[k] = v;
+        });
+
+        return hash;
+    }
+
+    function defineConst(obj, key, value) {
+        Object.defineProperty(obj, key, {
+            "configurable" : true,
+            "get" : (function (v) {
+                return function () {
+                    return v;
+                }
+            })(value)
+        });
+    }
+
     /* Constants */
     window.c = {
         "DEBUG"     : false,
 
-        "DOUBLEBUF" : false,
         "WIDTH"     : 960,
         "HEIGHT"    : 640,
 
@@ -22,11 +46,13 @@
                 S4() + "-" +
                 S4() + S4() + S4()
             );
-        })()
+        })(),
+
+        "HASH"      : parseHash()
     };
 
     // Helper to enable debug by setting a special hash in the URL.
-    if (document.location.hash === "#debug") {
+    if (typeof(window.c.HASH.debug) !== "undefined") {
         c.DEBUG = true;
         window.onReady(function () {
             enableDebug.defer(true);
@@ -34,45 +60,26 @@
     }
 
     window.addEventListener("hashchange", function (e) {
-        var debug = (document.location.hash === "#debug");
+        defineConst(window.c, "HASH", parseHash());
+
+        var debug = (typeof(window.c.HASH.debug) !== "undefined");
         enableDebug(debug);
-        try {
-            c.__defineGetter__("DEBUG", function () {
-                return debug;
-            });
-        }
-        catch (e) {
-            c.DEBUG = debug;
-        }
+        defineConst(window.c, "DEBUG", debug);
     });
 
     // Turn the `c` object into a hash of constants.
-    try {
-        Object.keys(c).forEach(function (key) {
-            if (typeof(c[key]) === "function") {
-                return;
-            }
+    Object.keys(window.c).forEach(function (key) {
+        if (typeof(window.c[key]) === "function") {
+            return;
+        }
 
-            c.__defineGetter__(
-                key,
-                (function (value) {
-                    return function () {
-                        return value
-                    };
-                })(c[key])
-            );
-        });
-    }
-    catch (e) {
-        // No getters? FAKE CONSTANTS!
-    }
+        defineConst(window.c, key, window.c[key]);
+    });
 
 
     // Game engine settings.
     me.sys.gravity = 0;
-    //me.sys.dirtyRegion = true; // Be fast!
     me.sys.preRender = true; // Be faster!
-    me.sys.useNativeAnimFrame = true; // Be fastest!
     me.sys.stopOnAudioError = false;
 
     function enableDebug(enable) {
